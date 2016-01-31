3b91f39475eeba1a4bb98aa8816139c576b828d9
Sam Jantz <sjantz0@gmail.com>
2016-01-30 22:25:14 -0600

    Now with more splash

--- js/game.js
+++ js/game.js
@@ -19,6 +19,7 @@ function preload() {
   game.load.spritesheet('saddestTooth', 'assets/saddestTooth.png', TILE_SIZE, TILE_SIZE);
   game.load.spritesheet('cavityTooth', 'assets/cavityTooth.png', TILE_SIZE, TILE_SIZE);
   game.load.spritesheet('toothbrush', 'assets/toothBrush.png', TILE_SIZE, TILE_SIZE);
+  game.load.spritesheet('splash', 'assets/splashScreen.png', 896, 640);
   game.load.image('gums', 'assets/gums.png');
   game.load.image('gumBlock', 'assets/gumTile.png');
   game.load.image('topLip', 'assets/topLips.png');
@@ -36,9 +37,10 @@ var toothbrush;
 var winningText;
 var brushingSound;
 var muteCleanToothSound;
+var splashIsUp = true;
+var splash, splashText, textTimer;
 function create() {
   mouse = new Phaser.Pointer(game, 0, Phaser.CURSOR);
-  game.physics.startSystem(Phaser.Physics.ARCADE);
   game.add.sprite(0, 0, 'realBg');
   game.add.sprite(0, 0, 'bg');
   brushingSound = game.add.audio('brushingSound');
@@ -48,18 +50,10 @@ function create() {
 
   var music = game.add.audio('bgMusic');
   game.sound.play('bgMusic', 1, true);
-  console.log(music.isPlaying);
 
   muteCleanToothSound = true;
 
   initGroups(game);
-  Teeth.enableBody = true;
-  generateTopTeeth();
-  generateBottomTeeth();
-  game.sound.play('levelStartSound');
-  animateTeeth();
-
-  muteCleanToothSound = false;
 
   game.add.sprite(0, 0, 'topLip');
   game.add.sprite(0, getGridPixel(6) + 36, 'bottomLip');
@@ -68,16 +62,45 @@ function create() {
   toothbrush = game.add.sprite(0, 0, 'toothbrush');
   toothbrush.animations.add('brush', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
 
+  splash = game.add.sprite(0,0, 'splash');
+  splash.animations.add('run', [0,1,2,3], 6, true);
+  splashText = game.add.text(0,0, 'Click To Start...', {fontSize:'30px', fill:'#FFF', stroke: '#000', strokeThickness:6, boundsAlignV:'middle', boundsAlignH:'center'});
+  splashText.setTextBounds(0, 235, 896, 100);
+  textTimer = setInterval(function(){
+    splashText.visible = !splashText.visible;
+  }, 500);
+
   winningText = game.add.text(0, 0, '', { fontSize: '50px', fill: '#FFF', stroke: '#000', strokeThickness: 6 , boundsAlignV: 'middle', boundsAlignH: 'center'})
   winningText.setTextBounds(0, 250 , 896, 100);
 }
 
 var frame = 0;
 function update() {
-  animateTeeth();
-  updateToothbrushPosition();
-  cleanTooth();
-  checkForWin();
+  if(!splashIsUp){
+    animateTeeth();
+    updateToothbrushPosition();
+    cleanTooth();
+    checkForWin();
+  } else {
+    splash.animations.play('run');
+    checkForGameStart();
+  }
+}
+
+
+function checkForGameStart(){
+  if(game.input.mousePointer.isDown){
+    generateTopTeeth();
+    generateBottomTeeth();
+    game.sound.play('levelStartSound');
+    animateTeeth();
+
+    muteCleanToothSound = false;
+    splashIsUp = false;
+    clearInterval(textTimer);
+    splashText.destroy();
+    splash.destroy();
+  }
 }
 
 function initGroups(game){
